7d83e8e9d65c3980d550b8ee40315c0fcab1f113
TobeShunta <shuntatobe.hal@gmail.com>
2017-10-19 12:56:45 +0900

    swingテスト
    未完

--- BluetoothController/Assets/Script/WiiTest.cs
+++ BluetoothController/Assets/Script/WiiTest.cs
@@ -7,19 +7,15 @@ using WiimoteApi;
 
 public class WiiTest : MonoBehaviour
 {
-	public WiimoteModel WmModel;
-	private Wiimote Wm; // wiiリモコン
-	private Vector3 WmOffset = Vector3.zero;
-	private Quaternion InitialRotation;
-
-	public RectTransform[] ir_dots;
-	public RectTransform[] ir_bb;
-	public RectTransform ir_pointer;
+	public WiimoteModel wmModel;				// モデル
+	private Wiimote wm;							// wiiリモコンハンドル
+	private Quaternion initialRotation;			// 角度初期値
+	private Vector3 wmpOffset = Vector3.zero;	// ジャイロオフセット
 
 	// 初期化処理
 	void Start()
 	{
-		InitialRotation = WmModel.rot.localRotation;
+		initialRotation = wmModel.rot.localRotation;
 	}
 
 	// 更新処理
@@ -29,81 +25,53 @@ public class WiiTest : MonoBehaviour
 		if (!WiimoteManager.HasWiimote()) { return; }
 
 		// 1Pを保存
-		Wm = WiimoteManager.Wiimotes[0];
-
-		int ret = Wm.ReadWiimoteData();
-
-		while (ret > 0)
+		wm = WiimoteManager.Wiimotes[0];
+		int ret;
+		do
 		{
-			if (ret > 0 && Wm.current_ext == ExtensionController.MOTIONPLUS)
+			// リモコンの情報を更新
+			ret = wm.ReadWiimoteData();
+
+			if (ret > 0 && wm.current_ext == ExtensionController.MOTIONPLUS)
 			{
-				Vector3 offset = new Vector3(-Wm.MotionPlus.PitchSpeed, Wm.MotionPlus.YawSpeed, Wm.MotionPlus.RollSpeed) /95.0f;
-				WmModel.rot.Rotate(offset, Space.Self);
+				Vector3 offset = new Vector3(-wm.MotionPlus.PitchSpeed,
+												wm.MotionPlus.YawSpeed,
+												wm.MotionPlus.RollSpeed) / 95f; // Divide by 95Hz (average updates per second from wiimote)
+				wmpOffset += offset;
+
+				wmModel.rot.Rotate(offset, Space.Self);
 			}
-			ret = Wm.ReadWiimoteData();
-		}
+		} while (ret > 0);
+
 		// ボタン降下情報
-		WmModel.a.enabled = Wm.Button.a;
-		WmModel.b.enabled = Wm.Button.b;
-		WmModel.one.enabled = Wm.Button.one;
-		WmModel.two.enabled = Wm.Button.two;
-		WmModel.d_up.enabled = Wm.Button.d_up;
-		WmModel.d_down.enabled = Wm.Button.d_down;
-		WmModel.d_left.enabled = Wm.Button.d_left;
-		WmModel.d_right.enabled = Wm.Button.d_right;
-		WmModel.plus.enabled = Wm.Button.plus;
-		WmModel.minus.enabled = Wm.Button.minus;
-		WmModel.home.enabled = Wm.Button.home;
+		wmModel.a.enabled = wm.Button.a;
+		wmModel.b.enabled = wm.Button.b;
+		wmModel.one.enabled = wm.Button.one;
+		wmModel.two.enabled = wm.Button.two;
+		wmModel.d_up.enabled = wm.Button.d_up;
+		wmModel.d_down.enabled = wm.Button.d_down;
+		wmModel.d_left.enabled = wm.Button.d_left;
+		wmModel.d_right.enabled = wm.Button.d_right;
+		wmModel.plus.enabled = wm.Button.plus;
+		wmModel.minus.enabled = wm.Button.minus;
+		wmModel.home.enabled = wm.Button.home;
+
 
 		Debug.Log(GetAccelVector());
 
+
 		// Wiiリモコンプラスでなければモデルは回転しない
-		if (Wm.current_ext != ExtensionController.MOTIONPLUS)
+		if (wm.current_ext != ExtensionController.MOTIONPLUS)
 		{
-			WmModel.rot.localRotation = InitialRotation;
+			wmModel.rot.localRotation = initialRotation;
 		}
-
-		if (ir_dots.Length < 4)
-		{
-			return;
-		}
-
-		float[,] ir = Wm.Ir.GetProbableSensorBarIR();
-		for (int i = 0; i < 2; i++)
-		{
-			float x = (float)ir[i, 0] / 1023f;
-			float y = (float)ir[i, 1] / 767f;
-			if (x == -1 || y == -1)
-			{
-				ir_dots[i].anchorMin = new Vector2(0, 0);
-				ir_dots[i].anchorMax = new Vector2(0, 0);
-			}
-
-			ir_dots[i].anchorMin = new Vector2(x, y);
-			ir_dots[i].anchorMax = new Vector2(x, y);
-
-			if (ir[i, 2] != -1)
-			{
-				int index = (int)ir[i, 2];
-				float xmin = (float)Wm.Ir.ir[index, 3] / 127f;
-				float ymin = (float)Wm.Ir.ir[index, 4] / 127f;
-				float xmax = (float)Wm.Ir.ir[index, 5] / 127f;
-				float ymax = (float)Wm.Ir.ir[index, 6] / 127f;
-				ir_bb[i].anchorMin = new Vector2(xmin, ymin);
-				ir_bb[i].anchorMax = new Vector2(xmax, ymax);
-			}
-		}
-
-		float[] pointer = Wm.Ir.GetPointingPosition();
-		ir_pointer.anchorMin = new Vector2(pointer[0], pointer[1]);
-		ir_pointer.anchorMax = new Vector2(pointer[0], pointer[1]);
 	}
 
 	// デバッグ
 	void OnGUI()
 	{
 		// 枠
-		GUI.Box(new Rect(0,0,0,Screen.height),"");
+		GUI.Box(new Rect(0, 0, 0, Screen.height), "");
 
 		GUILayout.BeginVertical(GUILayout.Width(300));
 		GUILayout.Label("WiimoteFound:" + WiimoteManager.HasWiimote());
@@ -115,32 +83,32 @@ public class WiiTest : MonoBehaviour
 		}
 
 		// 切断ボタン
-		if (GUILayout.Button("Cleanup") )
+		if (GUILayout.Button("Cleanup"))
 		{
 			Debug.Log("Wiiリモコンの接続を解除しました");
-			WiimoteManager.Cleanup(Wm);
-			Wm = null;
+			WiimoteManager.Cleanup(wm);
+			wm = null;
 		}
 
 		// 接続されていなかったらこれ以降を表示しない
-		if (Wm == null)
+		if (wm == null)
 		{
 			return;
 		}
 
 		// 外部コントローラー確認
-		GUILayout.Label("Extension: " + Wm.current_ext.ToString());
+		GUILayout.Label("Extension: " + wm.current_ext.ToString() + wm.Type);
 
 		// LEDチェック
 		GUILayout.BeginHorizontal();
 		for (int i = 0; i < 4; i++)
 		{
 			if (GUILayout.Button("" + i, GUILayout.Width(300 / 4)))
-				Wm.SendPlayerLED(i == 0, i == 1, i == 2, i == 3);
+				wm.SendPlayerLED(i == 0, i == 1, i == 2, i == 3);
 		}
 		if (GUILayout.Button("LED Reset", GUILayout.Width(300 / 4)))
 		{
-			Wm.SendPlayerLED(false,false,false,false);
+			wm.SendPlayerLED(false, false, false, false);
 		}
 		GUILayout.EndHorizontal();
 
@@ -148,25 +116,46 @@ public class WiiTest : MonoBehaviour
 		GUILayout.Label("振動");
 		if (GUILayout.Button("ON"))
 		{
-			Wm.RumbleOn = true;
-			Wm.SendStatusInfoRequest();
+			wm.RumbleOn = true;
+			wm.SendStatusInfoRequest();
 		}
 		if (GUILayout.Button("OFF"))
 		{
-			Wm.RumbleOn = false;
-			Wm.SendStatusInfoRequest();
+			wm.RumbleOn = false;
+			wm.SendStatusInfoRequest();
 		}
 
 		// 加速度
 		GUILayout.BeginHorizontal();
 		if (GUILayout.Button("But/Acc", GUILayout.Width(300 / 4)))
-			Wm.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL);
+			wm.SendDataReportMode(InputDataType.REPORT_BUTTONS);
 		if (GUILayout.Button("But/Ext8", GUILayout.Width(300 / 4)))
-			Wm.SendDataReportMode(InputDataType.REPORT_BUTTONS_EXT8);
+			wm.SendDataReportMode(InputDataType.REPORT_BUTTONS_EXT8);
 		if (GUILayout.Button("B/A/Ext16", GUILayout.Width(300 / 4)))
-			Wm.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
+			wm.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
 		if (GUILayout.Button("Ext21", GUILayout.Width(300 / 4)))
-			Wm.SendDataReportMode(InputDataType.REPORT_EXT21);
+			wm.SendDataReportMode(InputDataType.REPORT_EXT21);
+		GUILayout.EndHorizontal();
+
+		// モーションプラス設定
+		GUILayout.Label("WMP Attached: " + wm.wmp_attached);
+		if (GUILayout.Button("Request Identify WMP"))
+			wm.RequestIdentifyWiiMotionPlus();
+		if ((wm.wmp_attached || wm.Type == WiimoteType.PROCONTROLLER) && GUILayout.Button("Activate WMP"))
+			wm.ActivateWiiMotionPlus();
+		if ((wm.current_ext == ExtensionController.MOTIONPLUS ||
+			wm.current_ext == ExtensionController.MOTIONPLUS_CLASSIC ||
+			wm.current_ext == ExtensionController.MOTIONPLUS_NUNCHUCK) && GUILayout.Button("Deactivate WMP"))
+			wm.DeactivateWiiMotionPlus();
+
+		GUILayout.Label("Calibrate Accelerometer");
+		GUILayout.BeginHorizontal();
+		for (int x = 0; x < 3; x++)
+		{
+			AccelCalibrationStep step = (AccelCalibrationStep)x;
+			if (GUILayout.Button(step.ToString(), GUILayout.Width(100)))
+				wm.Accel.CalibrateAccel(step);
+		}
 		GUILayout.EndHorizontal();
 	}
 
@@ -176,7 +165,7 @@ public class WiiTest : MonoBehaviour
 		float accel_y;
 		float accel_z;
 
-		float[] accel = Wm.Accel.GetCalibratedAccelData();
+		float[] accel = wm.Accel.GetCalibratedAccelData();
 		accel_x = accel[0];
 		accel_y = -accel[2];
 		accel_z = -accel[1];
@@ -199,5 +188,14 @@ public class WiiTest : MonoBehaviour
 		public Renderer plus;
 		public Renderer minus;
 		public Renderer home;
+	}
+
+	// 振動関数
+	public bool Swing()
+	{
+		bool swing = false;
+
+
+		return swing;
 	}
 }
